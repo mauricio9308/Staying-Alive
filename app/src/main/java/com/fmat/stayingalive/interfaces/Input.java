59d492bd77cd99ab84b7167df8159d3f9d8f45e1
Kevin Cabrera <kevin.cabrera.argaez@gmail.com>
2014-09-24 14:26:35 -0500

    KeyboardHandler

--- app/src/main/java/com/fmat/stayingalive/interfaces/Input.java
+++ app/src/main/java/com/fmat/stayingalive/interfaces/Input.java
@@ -3,7 +3,26 @@ package com.fmat.stayingalive.interfaces;
 import java.util.List;
 
 public interface Input {
-	public boolean isKeyPressed(int keyCode);
+
+    public static class KeyEvent {
+        public static final int KEY_DOWN = 0;
+        public static final int KEY_UP = 1;
+        public int type;
+        public int keyCode;
+        public char keyChar;
+    }
+
+    public static class TouchEvent {
+        public static final int TOUCH_DOWN = 0;
+        public static final int TOUCH_UP = 1;
+        public static final int TOUCH_DRAG = 2;
+        public int type;
+        public int x, y;
+        public int pointer;
+    }
+
+
+    public boolean isKeyPressed(int keyCode);
 
 	public boolean isTouchDown(int pointer);
 
